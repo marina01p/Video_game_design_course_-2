f409d446833b89cde5cfd9fb072491e1b72e1a19
marina <marina.petici1@gmail.com>
2024-04-17 16:18:28 +0300

    add inventory UI

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class Inventory : MonoBehaviour
 {
@@ -9,13 +11,16 @@ public class Inventory : MonoBehaviour
     public List<InventoryItem> items = new List<InventoryItem>();
 
     // pentru UI
-    public GameObject inventoryUI;
+    public GameObject inventoryPanelUI;
+    public GameObject[] slots;
 
     void Start()
     {
-        inventoryUI.SetActive(false);
+        inventoryPanelUI.SetActive(false);
+        UpdateInventoryDisplay();
     }
 
+    
     public void AddItem(string itemTypeID)
     {
         InventoryItem foundItem = items.Find(item => item.itemTypeID == itemTypeID);
@@ -29,13 +34,14 @@ public class Inventory : MonoBehaviour
             items.Add(new InventoryItem(itemTypeID, 1));
         }
 
+        UpdateInventoryDisplay();
         OnInventoryChanged?.Invoke();
     }
 
     public void RemoveItem(string itemTypeID)
     {
-        InventoryItem foundItem = items.Find(inventoryItem => inventoryItem.itemTypeID == itemTypeID);
-        
+        InventoryItem foundItem = items.Find(item => item.itemTypeID == itemTypeID);
+    
         if (foundItem != null)
         {
             foundItem.quantity--;
@@ -44,19 +50,40 @@ public class Inventory : MonoBehaviour
                 items.Remove(foundItem);
             }
         }
+        UpdateInventoryDisplay();
     }
 
     void Update()
     {
+        // pentru UI
         if (Input.GetKeyDown(KeyCode.E))
         {
-            GetComponent<Inventory>().ToggleInventory();
+            GetComponent<Inventory>().ShowInventory();
+        }
+    }
+
+    void UpdateInventoryDisplay()
+    {
+        foreach (GameObject slot in slots)
+        {
+            slot.GetComponentInChildren<TextMeshProUGUI>().text = "";
+        }
+
+
+        foreach (InventoryItem item in items)
+        {
+            GameObject slot = Array.Find(slots, slot => slot.name == item.itemTypeID);
+            if (slot != null)
+            {
+                TextMeshProUGUI slotText = slot.GetComponentInChildren<TextMeshProUGUI>();
+                slotText.text = item.quantity.ToString();
+            }
         }
     }
 
     // pentru UI
-    public void ToggleInventory()
+    public void ShowInventory()
     {
-        inventoryUI.SetActive(!inventoryUI.activeSelf);
+        inventoryPanelUI.SetActive(!inventoryPanelUI.activeSelf);
     }
 }
